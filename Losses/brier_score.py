892673adea91aadcc6687e9f096f5d8889dd5813
Jinxu-Lin <jlin8272@uni.sydney.edu.au>
2024-08-29 18:15:34 +1000

    train

--- Losses/brier_score.py
+++ Losses/brier_score.py
@@ -49,9 +49,11 @@ class BrierScore(nn.Module):
         loss = torch.sum(squared_diff) / float(input.shape[0])
         return loss
     
+    
 class BrierScoreExp(nn.Module):
-    def __init__(self):
+    def __init__(self, temperature=1.0):
         super(BrierScoreExp, self).__init__()
+        self.temperature = temperature
 
     def forward(self, input, target):
         if input.dim()>2:
@@ -67,31 +69,13 @@ class BrierScoreExp(nn.Module):
         squared_diff = (target_one_hot - pt) ** 2
         
         loss = torch.sum(squared_diff) / float(input.shape[0])
-        loss = torch.exp(loss)
+    
+        loss = loss * torch.exp(
+            torch.clamp(loss.detach(), min=0, max=self.temperature) / (self.temperature + 1)
+        ) - loss
 
         return loss
     
-class BrierScoreExpMinusBrierScore(nn.Module):
-    def __init__(self):
-        super(BrierScoreExpMinusBrierScore, self).__init__()
-
-    def forward(self, input, target):
-        if input.dim()>2:
-            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1,1)
-        target_one_hot = torch.FloatTensor(input.shape).to(target.get_device())
-        target_one_hot.zero_()
-        target_one_hot.scatter_(1, target, 1)
-
-        pt = F.softmax(input)
-        squared_diff = (target_one_hot - pt) ** 2
-        
-        loss = torch.sum(squared_diff) / float(input.shape[0])
-        loss = torch.exp(loss) - loss
-
-        return loss
 
 class BSCELoss(nn.Module):
     def __init__(self, gamma=0, norm=1, size_average=False):
